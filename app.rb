0e8d011f430d4c2787b008ceabef66173ad4c781
Christopher Lubaway <asimplechris@gmail.com>
2014-10-14 14:50:10 -0700

    Adds routes directory, httparty, and dotenv Moves routes from app to dedicated directory Adds support for gems we know we must use

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@
 require 'bundler/setup'
 Bundler.require(:default)
 
-get '/' do
-  "hello job world"
+# Set . to pre-pend the load path
+$LOAD_PATH.unshift(File.expand_path('.'))
+
+# Allow the environment to be set by rack while gracefully catching heroku load error
+begin
+  require 'dotenv'
+  Dotenv.load(".env.#{ENV['RACK_ENV']}", ".env")
+rescue LoadError
+end
+
+Dir["routes/*.rb"].each do |file_to_load|
+  require file_to_load
 end
